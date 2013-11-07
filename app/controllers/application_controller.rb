109f17acc2acc0a54f0f930b087e95dde67b3bbf
Ryan Clarke <clarkie.ryan@gmail.com>
2013-11-07 02:37:22 +0000

    Changed header information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers, :set_csrf_cookie_for_ng
+  after_filter :set_access_control_headers
 
   #Adding in some basic exception control here for ActiveRecord
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
@@ -31,36 +30,10 @@ class ApplicationController < ActionController::Base
 
 # For all responses in this controller, return the CORS access control headers.
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+  after_filter
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost/'
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-# If this is a preflight OPTIONS request, then short-circuit the
-# request, return only the necessary headers and return an empty
-# text/plain.
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
-
-  def set_csrf_cookie_for_ng
-    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
-  end
-
-  protected
-
-  def verified_request?
-    super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
   end
 end
